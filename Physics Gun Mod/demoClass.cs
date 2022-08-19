5b3fa635da117ebe42a01e79db85f46b465d4109
Sivaguru Muralidharan <67368980+Kyonto@users.noreply.github.com>
2022-08-19 13:12:12 +0530

    Add files via upload

---
+++ Physics Gun Mod/demoClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using GTA.NaturalMotion;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Demo_Project
+{
+    public class demoClass : Script
+    {  
+        public demoClass()
+        {
+            this.Tick += onTick;
+            this.KeyUp += onKeyUp;
+            this.KeyDown += onKeyDown;
+        }
+        private void onTick(object sender, EventArgs e)
+        {
+        }
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+        }
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            
+            
+            if (e.KeyCode == Keys.O & GTA.Game.Player.IsAiming)
+            {
+                Entity ent = GTA.World.GetCrosshairCoordinates().HitEntity;
+                if (ent != null)
+                {
+                    ent.ApplyForce(Game.Player.Character.ForwardVector * 10);
+                }
+            }
+        }
+    }
+}
